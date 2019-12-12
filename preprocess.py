631deb49376ae241935b5b81270c5b49e6a81aa0
Yoav Navon <yanavon@uc.cl>
2019-12-11 23:01:21 -0300

    Add track feats preprocess

--- preprocess.py
+++ preprocess.py
@@ -4,6 +4,7 @@ import numpy as np
 from glob import glob
 from tqdm import tqdm
 import json
+from sklearn.preprocessing import StandardScaler
 
 
 def create_test_split(training_file, testing_file):
@@ -43,6 +44,33 @@ def get_item_idxs(input_path):
         json.dump(items, file)
 
 
+def get_track_feats(path):
+    tracks0 = pd.read_csv(path + 'track_features/tf_000000000000.csv')
+    tracks1 = pd.read_csv(path + 'track_features/tf_000000000001.csv')
+    items_idxs = json.load(open('item_idxs.json', 'r'))
+
+    mask = []
+    for i in tracks0['track_id']:
+        mask.append(i in items_idxs)
+        mask0 = np.array(mask)
+    tracks0 = tracks0[mask0]
+
+    mask = []
+    for i in tracks1['track_id']:
+        mask.append(i in items_idxs)
+        mask1 = np.array(mask)
+    tracks1 = tracks1[mask1]
+
+    track_feats = pd.concat([tracks0, tracks1])
+    track_feats = track_feats.drop('mode', axis=1)
+    track_feats['idx'] = track_feats['track_id'].apply(lambda x: items_idxs[x])
+    track_feats = track_feats.drop('track_id', axis=1)
+    track_feats = track_feats.set_index('idx')
+    track_feats_std = pd.DataFrame(StandardScaler().fit_transform(
+        track_feats), index=track_feats.index)
+    track_feats_std.to_csv('tracks_feats.csv', index=True)
+
+
 if __name__ == "__main__":
     training_file = 'data/training/log_3_20180827_000000000000.csv.gz'
     testing_file = 'data/log_1_20180715_000000000000.csv.gz'
@@ -50,3 +78,5 @@ if __name__ == "__main__":
 
     input_path = 'data/training'
     get_item_idxs(input_path)
+
+    get_track_feats('data/')
